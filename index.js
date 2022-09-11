482ddc6fadb48dff00ef5427288ce509689665b0
pulavarthiChaitanya <67095614+pulavarthiChaitanya@users.noreply.github.com>
2022-09-11 18:36:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+var n=document.querySelectorAll("button").length;
+function fun(trigger){
+    switch(trigger){
+        case 'w':
+            var audio=new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case  'a':
+            var audio=new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case 's':
+            var audio=new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        case 'd':
+            var audio=new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case 'j':
+            var audio=new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case  'k':
+            var audio=new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case  'l':
+            var audio=new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+
+        default:console.log("wrong input");
+    }
+}
+
+function eventanimation(trigger){
+    document.querySelector("."+trigger).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("."+trigger).classList.remove("pressed");
+    },100);
+}
+for(var i=0;i<n;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+        fun(this.innerHTML);
+        eventanimation(this.innerHTML);
+    });
+}
+
+document.addEventListener("keypress",function(event){
+    fun(event.key);
+    eventanimation(this.innerHTML);
+});
+
